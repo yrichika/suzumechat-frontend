18c3344d5faacc69b7623a6cdee7f68e95d483a3
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-02 08:20:18 +0900

    Changes to create a docker image for this app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // necessary to create docker image
 }
 
 module.exports = nextConfig
