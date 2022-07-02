b6c3d25ac0c808f1b90bc4052e783a207ce9580d
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-02 18:45:53 +0900

    added testing libraries and settings

---
+++ __tests__/example.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import NextPage from '../pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<NextPage />);
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
