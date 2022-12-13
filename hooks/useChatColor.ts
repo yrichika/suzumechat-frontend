f441d1b335e67fecb1efa60020b27b89495be3ba
yrichika <68413973+yrichika@users.noreply.github.com>
2022-12-13 20:53:18 +0900

    Fix chat usernames text color

--- hooks/useChatColor.ts
+++ hooks/useChatColor.ts
@@ -3,13 +3,11 @@ import { randomInt } from '@utils/UnsafeRandom'
 import { getClearTextColorForBg } from '@utils/Util'
 import { useEffect, useState } from 'react'
 
-// FIXME: not working: it's fine when testing but always returns 'text-back' on browser
 export default function useChatColor() {
-  const [color, setColor] = useState('')
+  const [color] = useState(colors[randomInt(0, 34)])
   const [nameTextColor, setNameTextColor] = useState('text-black')
 
   useEffect(() => {
-    setColor(colors[randomInt(0, 34)])
     setNameTextColor(getClearTextColorForBg(color))
   }, [])
 
