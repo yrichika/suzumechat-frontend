ebcf3fe139979b6932b43f8a7c7b103057e03764
yrichika <68413973+yrichika@users.noreply.github.com>
2022-12-25 09:52:25 +0900

    Fix http method

--- services/endChatService.ts
+++ services/endChatService.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export default async function endChatService(guestChannelToken: string) {
   const url =
     process.env.NEXT_PUBLIC_BACK_PREFIX + '/guest/endChat/' + guestChannelToken
-  return await axios.post(url)
+  return await axios.get(url)
 }
